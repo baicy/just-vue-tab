6c09503256911be5f4fd128f2a9fd97d423455b3
baicy <baicy0118@gmail.com>
2022-05-21 10:59:14 +0800

    vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    // 路径别名, 用@表示/src
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 8080, // 项目启动端口
+    // 配置热更新
+    hmr: {
+      host: '127.0.0.1',
+      port: 8080,
+    }
+  }
 })
